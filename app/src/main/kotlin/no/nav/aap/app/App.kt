e1c125cf5c3a9f872c4ae9130bcecc1e285e71fb
Robin <robin.tordly@nav.no>
2022-01-17 13:33:05 +0100

    Refaktorerer routing som egne extension functions
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Tor Idland <tor.idland@nav.no>

--- app/src/main/kotlin/no/nav/aap/app/App.kt
+++ app/src/main/kotlin/no/nav/aap/app/App.kt
@@ -10,48 +10,54 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.pipeline.*
 import no.nav.aap.app.modell.Aldersvurdering
 import no.nav.aap.app.modell.Oppgave
 import no.nav.aap.app.modell.Oppgaver
 import no.nav.aap.app.modell.Personident
 
 fun main() {
-    println("Hello world")
-    server()
+    embeddedServer(Netty, port = 8080, module = Application::server).start(wait = true)
 }
 
-private fun server() {
-    embeddedServer(Netty, port = 8080) {
-        install(ContentNegotiation) {
-            jackson {
-                enable(SerializationFeature.INDENT_OUTPUT)
-            }
-        }
-        val oppgaver = Oppgaver(
-            listOf(
-                Oppgave(1, Personident("11111111111"), 68),
-                Oppgave(2, Personident("12345678910"), 58),
-                Oppgave(3, Personident("01987654321"), 17),
-            )
+fun Application.server() {
+    install(ContentNegotiation) {
+        jackson { enable(SerializationFeature.INDENT_OUTPUT) }
+    }
+
+    val oppgaver = Oppgaver(
+        listOf(
+            Oppgave(1, Personident("11111111111"), 68),
+            Oppgave(2, Personident("12345678910"), 58),
+            Oppgave(3, Personident("01987654321"), 17),
         )
+    )
 
-        routing {
-            get("/healthy") {
-                call.respondText("Hello, world!")
-            }
-
-            get("/api/oppgaver") {
-                call.respond(oppgaver.oppgaver)
-            }
-
-            post("/api/vurderAlder") {
-                val aldersvurdering = call.receive<Aldersvurdering>()
-                when (aldersvurdering.erMellom18og67) {
-                    true -> log.info("hen for oppgaveId ${aldersvurdering.oppgaveId} er mellom 18 og 67")
-                    false -> log.info("hen for oppgaveId ${aldersvurdering.oppgaveId} er IKKE mellom 18 og 67")
-                }
-                call.respond(HttpStatusCode.Accepted)
-            }
-        }
-    }.start(wait = true)
+    routing {
+        api(oppgaver)
+        actuator()
+    }
 }
+
+fun Routing.api(oppgaver: Oppgaver) {
+    get("/api/oppgaver") {
+        call.respond(oppgaver.oppgaver)
+    }
+
+    post("/api/vurderAlder") {
+        val aldersvurdering = call.receive<Aldersvurdering>()
+        when (aldersvurdering.erMellom18og67) {
+            true -> log.info("hen for oppgaveId ${aldersvurdering.oppgaveId} er mellom 18 og 67")
+            false -> log.info("hen for oppgaveId ${aldersvurdering.oppgaveId} er IKKE mellom 18 og 67")
+        }
+        call.respond(HttpStatusCode.Accepted)
+    }
+}
+
+private fun Routing.actuator() {
+    get("/healthy") {
+        call.respondText("Hello, world!")
+    }
+}
+
+private val PipelineContext<Unit, ApplicationCall>.log get() = application.log
