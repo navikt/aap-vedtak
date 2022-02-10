1890c51c3a9c37475790d2b11ed03cee71f2e8a5
Richard Borge <richard.borge@nav.no>
2022-02-10 11:01:58 +0100

    Startet på modell av inntekt.
    Co-authored-by: Anders Østby <anders.ostby@nav.no>

--- domene/src/main/kotlin/no/nav/aap/domene/Søker.kt
+++ domene/src/main/kotlin/no/nav/aap/domene/Søker.kt
@@ -1,11 +1,18 @@
 package no.nav.aap.domene
 
+import no.nav.aap.domene.Beløp.Companion.summerBeløp
+import no.nav.aap.domene.Inntekt.Companion.inntektSiste3år
+import no.nav.aap.domene.Inntekt.Companion.inntektSisteÅr
 import no.nav.aap.domene.Sak.Companion.toFrontendSak
 import no.nav.aap.domene.entitet.Fødselsdato
 import no.nav.aap.domene.entitet.Personident
 import no.nav.aap.hendelse.LøsningParagraf_11_2
 import no.nav.aap.hendelse.LøsningParagraf_11_5
 import no.nav.aap.hendelse.Søknad
+import java.time.LocalDate
+import java.time.Year
+import java.time.YearMonth
+import kotlin.math.max
 
 class Søker(
     private val personident: Personident,
@@ -40,4 +47,47 @@ class Søker(
             .filter { it.personident == personident }
             .flatMap(Søker::toFrontendSaker)
     }
+}
+
+internal class Inntektshistorikk(
+    private val inntekter: List<Inntekt>
+) {
+
+    fun beregnGrunnlag(beregningsdato: LocalDate) {}
+
+    private fun inntektSisteÅr(år: Year): List<Inntekt> = inntekter.inntektSisteÅr(år)
+
+    private fun inntektSiste3år(år: Year): List<Inntekt> = inntekter.inntektSiste3år(år)
+
+}
+
+internal class Grunnlagsberegning(
+    private val grunnlag: Beløp,
+    private val inntektSisteÅr: List<Inntekt>,
+    private val inntektSiste3år: List<Inntekt>
+)
+
+internal class Inntekt(
+    private val arbeidsgiver: Arbeidsgiver,
+    private val inntekstmåned: YearMonth,
+    private val beløp: Beløp
+) {
+    companion object {
+        internal fun Iterable<Inntekt>.inntektSisteÅr(år: Year) = filter { it.inntekstmåned.year == år.value }
+        internal fun Iterable<Inntekt>.inntektSiste3år(år: Year) = (0..2).map { år.minusYears(it.toLong()) }.flatMap { inntektSisteÅr(it) }
+        internal fun Iterable<Inntekt>.summerInntekt() = map { it.beløp }.summerBeløp()
+    }
+
+}
+
+internal class Beløp(
+    private val beløp: Double
+) {
+    companion object {
+        fun Iterable<Beløp>.summerBeløp() = sumOf { it.beløp }
+    }
+}
+
+internal class Arbeidsgiver {
+
 }
