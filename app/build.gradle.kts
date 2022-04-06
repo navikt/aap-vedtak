99ab86c5ffbc37f90de4e6a61b03c82b03426beb
Anders Østby <anders.ostby@nav.no>
2022-04-06 11:01:25 +0200

    Bumper kotlin, gradle og andre avhengigheter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,10 +28,10 @@ dependencies {
     implementation("no.nav.aap.avro:inntekter:0.0.2")
     implementation("no.nav.aap.avro:medlem:1.1.6")
 
-    implementation("no.nav.security:token-validation-ktor:2.0.13")
-    implementation("no.nav.security:token-client-core:2.0.13")
+    implementation("no.nav.security:token-validation-ktor:2.0.14")
+    implementation("no.nav.security:token-client-core:2.0.14")
 
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.0.3")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.0.4")
 
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("io.ktor:ktor-jackson:1.6.8")
@@ -44,7 +44,7 @@ dependencies {
             because("Mac M1")
         }
     }
-    implementation("io.confluent:kafka-streams-avro-serde:7.0.1") {
+    implementation("io.confluent:kafka-streams-avro-serde:7.1.0") {
         exclude("org.apache.kafka", "kafka-clients")
     }
 
@@ -53,7 +53,7 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("io.ktor:ktor-server-test-host:1.6.8")
-    testImplementation("no.nav.security:mock-oauth2-server:0.4.3")
+    testImplementation("no.nav.security:mock-oauth2-server:0.4.4")
     // used to override env var runtime
     testImplementation("uk.org.webcompere:system-stubs-jupiter:2.0.1")
     testImplementation("org.apache.kafka:kafka-streams-test-utils:3.1.0")
