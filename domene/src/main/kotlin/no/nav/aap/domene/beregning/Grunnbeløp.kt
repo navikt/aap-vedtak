f1ecc0b24d94ffdcadc7a137209498ad1071d609
Anders Østby <anders.ostby@nav.no>
2022-05-23 10:39:44 +0200

    Oppdaterer grunnbeløp

--- domene/src/main/kotlin/no/nav/aap/domene/beregning/Grunnbeløp.kt
+++ domene/src/main/kotlin/no/nav/aap/domene/beregning/Grunnbeløp.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.domene.beregning
 
+import no.nav.aap.domene.beregning.Beløp.Companion.beløp
 import no.nav.aap.domene.beregning.Grunnbeløp.Element.Companion.beløpJustertFor6G
 import no.nav.aap.domene.beregning.Grunnbeløp.Element.Companion.finnGrunnlagsfaktor
 import no.nav.aap.domene.beregning.Grunnbeløp.Element.Companion.justerInntekt
@@ -9,40 +10,47 @@ import java.time.Year
 
 internal object Grunnbeløp {
     private val grunnbeløp = listOf(
-        Element(LocalDate.of(2021, 5, 1), Beløp(106399.0), Beløp(104716.0)),
-        Element(LocalDate.of(2020, 5, 1), Beløp(101351.0), Beløp(100853.0)),
-        Element(LocalDate.of(2019, 5, 1), Beløp(99858.0), Beløp(98866.0)),
-        Element(LocalDate.of(2018, 5, 1), Beløp(96883.0), Beløp(95800.0)),
-        Element(LocalDate.of(2017, 5, 1), Beløp(93634.0), Beløp(93281.0)),
-        Element(LocalDate.of(2016, 5, 1), Beløp(92576.0), Beløp(91740.0)),
-        Element(LocalDate.of(2015, 5, 1), Beløp(90068.0), Beløp(89502.0)),
-        Element(LocalDate.of(2014, 5, 1), Beløp(88370.0), Beløp(87328.0)),
-        Element(LocalDate.of(2013, 5, 1), Beløp(85245.0), Beløp(84204.0)),
-        Element(LocalDate.of(2012, 5, 1), Beløp(82122.0), Beløp(81153.0)),
-        Element(LocalDate.of(2011, 5, 1), Beløp(79216.0), Beløp(78024.0)),
-        Element(LocalDate.of(2010, 5, 1), Beløp(75641.0), Beløp(74721.0)),
-        Element(LocalDate.of(2009, 5, 1), Beløp(72881.0), Beløp(72006.0)),
-        Element(LocalDate.of(2008, 5, 1), Beløp(70256.0), Beløp(69108.0)),
-        Element(LocalDate.of(2007, 5, 1), Beløp(66812.0), Beløp(65505.0)),
-        Element(LocalDate.of(2006, 5, 1), Beløp(62892.0), Beløp(62161.0)),
-        Element(LocalDate.of(2005, 5, 1), Beløp(60699.0), Beløp(60059.0)),
-        Element(LocalDate.of(2004, 5, 1), Beløp(58778.0), Beløp(58139.0)),
-        Element(LocalDate.of(2003, 5, 1), Beløp(56861.0), Beløp(55964.0)),
-        Element(LocalDate.of(2002, 5, 1), Beløp(54170.0), Beløp(53233.0)),
-        Element(LocalDate.of(2001, 5, 1), Beløp(51360.0), Beløp(50603.0)),
-        Element(LocalDate.of(2000, 5, 1), Beløp(49090.0), Beløp(48377.0)),
-        Element(LocalDate.of(1999, 5, 1), Beløp(46950.0), Beløp(46423.0)),
-        Element(LocalDate.of(1998, 5, 1), Beløp(45370.0), Beløp(44413.0)),
-        Element(LocalDate.of(1997, 5, 1), Beløp(42500.0), Beløp(42000.0)),
-        Element(LocalDate.of(1996, 5, 1), Beløp(41000.0), Beløp(40410.0)),
-        Element(LocalDate.of(1995, 5, 1), Beløp(39230.0), Beløp(38847.0))
+        Element(2022, 5, 1, 111477, 109784),
+        Element(2021, 5, 1, 106399, 104716),
+        Element(2020, 5, 1, 101351, 100853),
+        Element(2019, 5, 1, 99858, 98866),
+        Element(2018, 5, 1, 96883, 95800),
+        Element(2017, 5, 1, 93634, 93281),
+        Element(2016, 5, 1, 92576, 91740),
+        Element(2015, 5, 1, 90068, 89502),
+        Element(2014, 5, 1, 88370, 87328),
+        Element(2013, 5, 1, 85245, 84204),
+        Element(2012, 5, 1, 82122, 81153),
+        Element(2011, 5, 1, 79216, 78024),
+        Element(2010, 5, 1, 75641, 74721),
+        Element(2009, 5, 1, 72881, 72006),
+        Element(2008, 5, 1, 70256, 69108),
+        Element(2007, 5, 1, 66812, 65505),
+        Element(2006, 5, 1, 62892, 62161),
+        Element(2005, 5, 1, 60699, 60059),
+        Element(2004, 5, 1, 58778, 58139),
+        Element(2003, 5, 1, 56861, 55964),
+        Element(2002, 5, 1, 54170, 53233),
+        Element(2001, 5, 1, 51360, 50603),
+        Element(2000, 5, 1, 49090, 48377),
+        Element(1999, 5, 1, 46950, 46423),
+        Element(1998, 5, 1, 45370, 44413),
+        Element(1997, 5, 1, 42500, 42000),
+        Element(1996, 5, 1, 41000, 40410),
+        Element(1995, 5, 1, 39230, 38847)
     )
 
     private class Element(
-        private val dato: LocalDate,
-        private val beløp: Beløp,
-        private val gjennomsnittBeløp: Beløp
+        år: Int,
+        måned: Int,
+        dag: Int,
+        beløp: Number,
+        gjennomsnittBeløp: Number
     ) {
+        private val dato: LocalDate = LocalDate.of(år, måned, dag)
+        private val beløp: Beløp = beløp.beløp
+        private val gjennomsnittBeløp: Beløp = gjennomsnittBeløp.beløp
+
         companion object {
             fun Iterable<Element>.justerInntekt(beregningsdato: LocalDate, grunnlagsfaktor: Grunnlagsfaktor) =
                 grunnlagsfaktor * finnGrunnbeløpForDato(beregningsdato).beløp
