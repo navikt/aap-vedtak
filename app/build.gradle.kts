b07986380afebcebe8d91f3af5183bb466562cdc
Anders Østby <anders.ostby@nav.no>
2022-05-23 09:35:32 +0200

    Bumper avhengigheter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ application {
 dependencies {
     implementation(project(":domene"))
 
-    implementation("com.github.navikt.aap-libs:ktor-utils:0.1.2")
-    implementation("com.github.navikt.aap-libs:kafka:0.1.2")
-    testImplementation("com.github.navikt.aap-libs:kafka-test:0.1.2")
+    implementation("com.github.navikt.aap-libs:ktor-utils:0.1.4")
+    implementation("com.github.navikt.aap-libs:kafka:0.1.4")
+    testImplementation("com.github.navikt.aap-libs:kafka-test:0.1.4")
 
-    implementation("com.github.navikt.aap-avro:medlem:3.0.7")
+    implementation("com.github.navikt.aap-avro:medlem:3.0.9")
 
     implementation("io.ktor:ktor-server-core:2.0.1")
     implementation("io.ktor:ktor-server-netty:2.0.1")
@@ -22,13 +22,13 @@ dependencies {
     implementation("io.ktor:ktor-serialization-jackson:2.0.1")
     implementation("io.ktor:ktor-server-metrics-micrometer:2.0.1")
 
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.0")
 
     implementation("ch.qos.logback:logback-classic:1.2.11")
-    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.1.1")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.2")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
 
     testImplementation(kotlin("test"))
 
