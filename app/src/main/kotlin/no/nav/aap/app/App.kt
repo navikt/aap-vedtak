7d92dbce46fd339fbe50262faf35618f81e54516
Anders Østby <anders.ostby@nav.no>
2022-06-17 09:25:48 +0200

    DevTool-endepunkt for å slette alle søkere i state store
    Co-authored-by: Richard Borge <richard.borge@nav.no>

--- app/src/main/kotlin/no/nav/aap/app/App.kt
+++ app/src/main/kotlin/no/nav/aap/app/App.kt
@@ -15,7 +15,6 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlinx.coroutines.delay
-import no.nav.aap.app.kafka.SØKERE_STORE_NAME
 import no.nav.aap.app.kafka.Tables
 import no.nav.aap.app.kafka.Topics
 import no.nav.aap.app.modell.JsonSøknad
@@ -30,6 +29,7 @@ import no.nav.aap.kafka.streams.store.scheduleMetrics
 import no.nav.aap.ktor.config.loadConfig
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.KTable
@@ -41,6 +41,7 @@ import org.apache.kafka.streams.processor.api.Record
 import org.apache.kafka.streams.state.KeyValueStore
 import org.apache.kafka.streams.state.ValueAndTimestamp
 import org.slf4j.LoggerFactory
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
@@ -80,6 +81,7 @@ internal fun topology(registry: MeterRegistry): Topology {
         .produce(Tables.søkere)
 
     søkerKTable.scheduleCleanup(Tables.søkere, 1.seconds, søkereToDelete)
+    søkerKTable.purge(Tables.søkere, 1.seconds, purgeFlag, purgeListeners)
     søkerKTable.scheduleMetrics(Tables.søkere, 2.minutes, registry)
 
     streams.søknadStream(søkerKTable)
@@ -107,6 +109,8 @@ private fun Routing.actuator(prometheus: PrometheusMeterRegistry, kafka: KStream
 }
 
 val søkereToDelete: MutableList<String> = mutableListOf()
+val purgeListeners: MutableList<(String) -> Unit> = mutableListOf()
+val purgeFlag: AtomicBoolean = AtomicBoolean(false)
 
 private fun Routing.devTools(kafka: KStreams, config: KafkaConfig) {
     val søkerProducer = kafka.createProducer(config, Topics.søkere)
@@ -128,6 +132,22 @@ private fun Routing.devTools(kafka: KStreams, config: KafkaConfig) {
         }
 
         call.respondText("Søknad og søker med ident $personident slettes!")
+    }
+
+    get("/deleteAll}") {
+        purgeListeners.add { personident ->
+            søknadProducer.produce(Topics.søknad, personident, null).also {
+                secureLog.info("produced [${Topics.søknad}] [$personident] [tombstone]")
+            }
+
+            søkerProducer.produce(Topics.søkere, personident, null).also {
+                søkereToDelete.add(personident)
+                secureLog.info("produced [${Topics.søkere}] [$personident] [tombstone]")
+            }
+        }
+        purgeFlag.set(true)
+
+        call.respondText("Sletter alle søknader og søkere!")
     }
 
     get("/søknad/{personident}") {
@@ -152,3 +172,44 @@ private fun Routing.devTools(kafka: KStreams, config: KafkaConfig) {
         call.respondText("Søknad $søknad mottatt!")
     }
 }
+
+private typealias PurgeListener<K> = (K) -> Unit
+
+private class StateStorePurger<K, V>(
+    private val table: Table<V>,
+    private val interval: Duration,
+    private val purgeFlag: AtomicBoolean,
+    private val purgeListeners: MutableList<PurgeListener<K>>,
+) : Processor<K, V, Void, Void> {
+    override fun process(record: Record<K, V>) {}
+
+    override fun init(context: ProcessorContext<Void, Void>) {
+        val store = context.getStateStore<KeyValueStore<K, ValueAndTimestamp<V>>>(table.stateStoreName)
+        context.schedule(interval.toJavaDuration(), PunctuationType.WALL_CLOCK_TIME) {
+            if (purgeFlag.getAndSet(false)) {
+                val keyValueIterator = store.all()
+                while (keyValueIterator.hasNext()) {
+                    val next: KeyValue<K, ValueAndTimestamp<V>> = keyValueIterator.next()
+                    purgeListeners.forEach { listener -> listener(next.key) }
+                    keyValueIterator.remove()
+                }
+                purgeListeners.clear()
+            }
+        }
+    }
+
+    private companion object {
+        private val secureLog = LoggerFactory.getLogger("secureLog")
+    }
+}
+
+fun <K, V> KTable<K, V>.purge(
+    table: Table<V>,
+    interval: Duration,
+    purgeFlag: AtomicBoolean,
+    purgeListeners: MutableList<PurgeListener<K>>,
+) = toStream().process(
+    ProcessorSupplier { StateStorePurger(table, interval, purgeFlag, purgeListeners) },
+    named("purge-${table.stateStoreName}"),
+    table.stateStoreName
+)
