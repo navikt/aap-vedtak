0726eb79f2590a6922bd177c70b52bcba6719417
Robin <robin.tordly@nav.no>
2022-08-26 11:35:36 +0200

    forsøk på å publisere jitpack

--- dto-kafka/build.gradle.kts
+++ dto-kafka/build.gradle.kts
@@ -1,5 +1,33 @@
+plugins {
+    `maven-publish`
+    `java-library`
+}
+
+
 dependencies {
     api(project(":domene"))
     api("com.github.navikt.aap-libs:kafka:3.1.11")
     testImplementation(kotlin("test"))
+}
+
+group = "com.github.navikt"
+version = "1.0.0-SNAPSHOT"
+
+tasks {
+    withType<Jar> {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+}
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.name
+            from(components["java"])
+        }
+    }
 }
