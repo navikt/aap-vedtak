31322cf3bed53710542b916b764e70813b17d00d
Robin <robin.tordly@nav.no>
2022-09-01 12:51:59 +0200

    midlertidig bruk kopi av kafkadto

--- dto-kafka/src/main/kotlin/no/nav/aap/dto/kafka/SykepengedagerKafkaDto.kt
+++ dto-kafka/src/main/kotlin/no/nav/aap/dto/kafka/SykepengedagerKafkaDto.kt
@@ -2,7 +2,11 @@ package no.nav.aap.dto.kafka
 
 import java.time.LocalDate
 
-data class SykepengedagerKafkaDto(val response: Response?) {
+// TODO: flytt til aap-sykepengedager
+data class SykepengedagerKafkaDto(
+    val response: Response?,
+    val version: Int = 1,
+) {
     data class Response(
         val gjenståendeSykedager: Int,
         val foreløpigBeregnetSluttPåSykepenger: LocalDate,
