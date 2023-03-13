ba674cc0c90e37fab7fa0a1815ee8a643d5edee3
Robin <robin.tordly@nav.no>
2023-03-13 14:50:25 +0100

    Fjern mocking av andre ytelser, mockes allerede i egen app
    Co-authored-by: Niro Karunakaran <niro.karunakaran@nav.no>

--- app/main/vedtak/stream/AndreFolketrygdytelserStream.kt
+++ app/main/vedtak/stream/AndreFolketrygdytelserStream.kt
@@ -6,19 +6,15 @@ import no.nav.aap.dto.kafka.AndreFolketrygdytelserKafkaDto
 import no.nav.aap.dto.kafka.SøkereKafkaDtoHistorikk
 import no.nav.aap.kafka.streams.v2.KTable
 import no.nav.aap.kafka.streams.v2.Topology
-import no.nav.aap.kafka.streams.v2.stream.ConsumedKStream
 import vedtak.kafka.Topics
 import vedtak.kafka.buffer
 
 internal fun Topology.andreFolketrygdytelserStream(søkere: KTable<SøkereKafkaDtoHistorikk>) {
     consume(Topics.andreFolketrygdsytelser)
-        .branch({ ytelser -> ytelser.response != null }) { stream ->
-            stream
-                .joinWith(søkere, søkere.buffer)
-                .map(håndterAndreFolketrygdytelser)
-                .produce(Topics.søkere, søkere.buffer) { it }
-        }
-        .branch({ ytelser -> ytelser.response == null }, ::mockResponse)
+        .filter { value -> value.response != null }
+        .joinWith(søkere, søkere.buffer)
+        .map(håndterAndreFolketrygdytelser)
+        .produce(Topics.søkere, søkere.buffer) { it }
 }
 
 private val håndterAndreFolketrygdytelser =
@@ -27,22 +23,3 @@ private val håndterAndreFolketrygdytelser =
         val (endretSøker) = andreFolketrygdytelser.toModellApi().håndter(søker)
         endretSøker.toSøkereKafkaDtoHistorikk(gammeltSekvensnummer)
     }
-
-private fun mockResponse(stream: ConsumedKStream<AndreFolketrygdytelserKafkaDto>) {
-    stream
-        .filterKey { ident -> ident != "123" } // 123 brukes i testene som mocker selv
-        .filter { ytelser -> ytelser.response == null }
-        .map { ytelser ->
-            ytelser.copy(
-                response = AndreFolketrygdytelserKafkaDto.Response(
-                    svangerskapspenger = AndreFolketrygdytelserKafkaDto.Response.Svangerskapspenger(
-                        fom = null,
-                        tom = null,
-                        grad = null,
-                        vedtaksdato = null,
-                    )
-                )
-            )
-        }
-        .produce(Topics.andreFolketrygdsytelser)
-}
